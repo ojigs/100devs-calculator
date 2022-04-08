f08897900f818cf85c60f5e9660a1019727a25c7
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2022-04-08 16:21:59 +0100

    Subtantially completed calculator but using eval function

--- js/main.js
+++ js/main.js
@@ -7,68 +7,93 @@
 //A reset function that clears all inputs from the display
 
 //Variabble to store DOM elements
-let inputOutput = document.getElementById('#result');
-let decimal = document.getElementById('#decimal');
-let clear = document.getElementById('#clear');
-let num = document.querySelector('.num');
-let operator = document.querySelector('.operator');
+let inputOutput = document.getElementById('result');
+let decimal = document.getElementById('decimal');
+let clear = document.getElementById('clear');
+let num = document.querySelectorAll('.num')
+let operator = document.querySelectorAll('.operator');
 
 //Variable to hold temporary values
-let currVal = 0;
-let pendVal;
+let currVal = '0';
+let preVal;
 let arrayVal = [];
 
 //Add event listeners to the number button
 for (let i = 0; i < num.length; i++) {
-    num[i].addEventListener('click')
+    num[i].addEventListener('click', updateCurrent)
 }
 for (let i = 0; i < operator.length; i++) {
-    operator[i].addEventListener('click')
+    operator[i].addEventListener('click', calculate)
 }
 
 //A function to update the current value
 //We use an event.target object property to obtain the values of whatever element triggered the event
 function updateCurrent(e) {
-    let btnContent = e.target.innerText; 
-    if (currVal == 0) {
+    let btnContent = e.target.innerText;
+    if (currVal == '0') {
         currVal = ''
     }
     currVal += btnContent;
-    inputOutput.innerText = currVal;
+    inputOutput.value = currVal; 
 }
 
 //Our main calculator function
-function calculate() {
+function calculate(e) {
     switch (e.target.innerText) {
         case '+': {
-            pendVal = currVal;
-            currVal = 0;
-            arrayVal.push(pendVal, '+'); 
+            preVal = currVal;
+            inputOutput.value = currVal + '+'
+            currVal = '0'
+            updateCurrent(e)
+            arrayVal.push(preVal, '+'); 
             break;
         }
         case '-': {
-            pendVal = currVal;
-            currVal = 0;
-            arrayVal.push(pendVal, '-'); 
+            preVal = currVal;
+            inputOutput.value = currVal + '-'
+            currVal = '0';
+            arrayVal.push(preVal, '-'); 
             break;
         }
         case 'x': {
-            pendVal = currVal;
-            currVal = 0;
-            arrayVal.push(pendVal, '*'); 
+            preVal = currVal;
+            inputOutput.value = currVal + 'x'
+            currVal = '0';
+            arrayVal.push(preVal, '*'); 
             break;
         }
         case '/': {
-            pendVal = currVal;
-            currVal = 0;
-            arrayVal.push(pendVal, '/'); 
+            preVal = currVal;
+            inputOutput.value = currVal + '/'
+            currVal = '0';
+            arrayVal.push(preVal, '/'); 
             break;
         }
         case '=': {
+            arrayVal.push(currVal)
             let result = eval( arrayVal.join('') )
+            currVal = result + ''
+            inputOutput.value = currVal
+            currVal = '0'
             arrayVal = []
-            inputOutput.innerText = result
             break;
         }
+        default: break;
+    }
+}
+
+//A function to clear everything
+clear.onclick = function() {
+    currVal = '0';
+    delete preVal;
+    arrayVal = [];
+    inputOutput.value = currVal
+}
+
+//function to insert decimal point
+decimal.onclick = function() {
+    if (!currVal.includes('.')) {
+        currVal += '.'
+        inputOutput.value = currVal
     }
 }
